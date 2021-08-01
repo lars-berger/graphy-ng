9f8185be900af3ed90b27368eb30d1dd93df5008
Lars <lars.berger@yahoo.com>
2021-08-01 23:26:16 +0200

    feat(lib): omit private properties from node and edge template contexts

--- lib/src/graph/models/edge-template-context.model.ts
+++ lib/src/graph/models/edge-template-context.model.ts
@@ -1,6 +1,8 @@
 import { TransformedEdge } from './transformed-edge.model';
 
+type EdgeContext<T> = TransformedEdge<T>;
+
 /** The context object passed to edge templates. */
 export interface EdgeTemplateContext<T> {
-  $implicit: TransformedEdge<T>;
+  $implicit: EdgeContext<T>;
 }
--- lib/src/graph/models/node-template-context.model.ts
+++ lib/src/graph/models/node-template-context.model.ts
@@ -1,6 +1,8 @@
 import { TransformedNode } from './transformed-node.model';
 
+type NodeContext<T> = Pick<TransformedNode<T>, 'id' | 'data'>;
+
 /** The context object passed to node templates. */
 export interface NodeTemplateContext<T> {
-  $implicit: TransformedNode<T>;
+  $implicit: NodeContext<T>;
 }
